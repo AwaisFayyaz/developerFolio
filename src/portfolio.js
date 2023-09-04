894d6d3de0a885a887d9cd64d951bad1a37d87ae
Awais Fayyaz <awais.fayyaz@nxb.com.pk>
2023-09-04 10:29:39 +0500

    Changed name and some description. initial changes

--- src/portfolio.js
+++ src/portfolio.js
@@ -20,10 +20,10 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Saad Pasta",
-  title: "Hi all, I'm Saad",
+  username: "Awais Fayyaz",
+  title: "Hi all, I'm Awais",
   subTitle: emoji(
-    "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
+    "A passionate iOS Software Developer 🚀 having an experience of building iOS applications with Objective C, Swift, SwiftUI, UIKit,  some other cool libraries and frameworks."
   ),
   resumeLink:
     "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
